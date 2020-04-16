771c098266a982758c475f7f22cd46a160a7e22d
Michael Silva <msilva@aggienetwork.com>
2020-04-16 15:59:14 -0500

    added drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -170,6 +170,60 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log($(this).children());
+  },
+  deactivate: function(event) {
+    console.log($(this).children());
+  },
+  over: function(event) {
+    console.log($(this).children());
+  },
+  out: function(event) {
+    console.log($(this).children());
+  },
+  update: function(event) {
+    // array to store the task data in
+var tempArr = [];
+
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+});
+
+console.log(tempArr);
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+    }
+  }
+);
+
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -179,7 +233,21 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
-
 
